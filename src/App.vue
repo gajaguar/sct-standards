ce7255d48bd93f52d8f9399cb05621150bf86270
G.A.JAGUAR <dev.gajaguar@gmail.com>
2020-05-13 17:48:11 -0500

    Implement new components and utilities

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <app-bar />
+    <v-content id="content">
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="stretch"
+          justify="center"
+        >
+          <v-col class="text-center">
+            <app-alert />
+            <v-card>
+              <v-card-title>
+                <v-text-field
+                  append-icon="search"
+                  autofocus
+                  clearable
+                  hide-details
+                  label="Buscar"
+                  outlined
+                  v-model="search"
+                ></v-text-field>
+              </v-card-title>
+                <app-table
+                  :headers="headers"
+                  :items="standards"
+                  :search="search"
+                />
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import AppAlert from './components/AppAlert.vue'
+import AppBar from './components/AppBar.vue'
+import AppTable from './components/AppTable.vue'
+import headers from './utils/headers'
+import standards from './utils/standards'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld,
+    AppAlert,
+    AppBar,
+    AppTable
   },
-};
+
+  data: () => ({
+    headers,
+    search: '',
+    standards
+  }),
+
+  created() {
+    this.$vuetify.theme.dark = false
+  }
+}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="sass">
+  #content
+    margin-top: 128px
 </style>
