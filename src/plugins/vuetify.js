07acbae7c6f3511e870c65cb981859859fbd2e78
G.A.JAGUAR <dev.gajaguar@gmail.com>
2020-05-13 17:46:16 -0500

    Update lint rules

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,15 +1,15 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
-import es from 'vuetify/es5/locale/es';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import es from 'vuetify/es5/locale/es'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
   lang: {
     locales: { es },
-    current: 'es',
+    current: 'es'
   },
   icons: {
-    iconfont: 'md',
-  },
-});
+    iconfont: 'md'
+  }
+})
